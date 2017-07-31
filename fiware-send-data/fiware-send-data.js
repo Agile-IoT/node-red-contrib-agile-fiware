cf7b84dacbb9b192cfa0a8b49f7493cdca9d837f
Román SG <roman.sosa@atos.net>
2017-07-31 13:38:13 +0100

    Fix license header according to AGILE guidelines

--- fiware-send-data/fiware-send-data.js
+++ fiware-send-data/fiware-send-data.js
@@ -1,12 +1,12 @@
 /*******************************************************************************
-* Copyright (c) 2017 Atos
+* Copyright (c) 2017 Atos, and others
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v1.0
 * which accompanies this distribution, and is available at
 * http://www.eclipse.org/legal/epl-v10.html
 *
 * Contributors:
-* Román Sosa - initial implementation
+* Atos - initial implementation
 *******************************************************************************/
 module.exports = function(RED) {
     //use this module to make requests
