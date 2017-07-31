ff2e64c3e065d148235c0a2739582012aa941efc
Román SG <rosogon@users.noreply.github.com>
2017-07-31 14:15:21 +0100

    Added missing copyright and EPL license file (#4)
    Added missing copyright and EPL license file

--- fiware-send-data/fiware-send-data.js
+++ fiware-send-data/fiware-send-data.js
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2017 Atos, and others
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* Atos - initial implementation
+*******************************************************************************/
 module.exports = function(RED) {
     //use this module to make requests
     var d = require('debug')('fiware')
